7e350352d3a0903bfc23830f95a038179cf93548
sys0502 <location0@naver.com>
2023-05-08 17:43:19 +0900

    ex commit

---
+++ home_page.c
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> open_source_work </title>
+        <link rel = "stylesheet" href = "home_page1.css">
+        <link rel = "stylesheet" href = "home_page2.css">
+        <style>
+        
+            .nav {
+                height: 70px;
+                border-bottom: 1px solid black;
+                display: flex;
+                align-items: center;
+                background-color: mediumaquamarine
+            }
+
+            .nav-right-items  {
+                display: flex;
+                margin-left: auto;
+            }
+            
+            .nav-item {
+                margin-left: 20px;
+                font-size: 30;
+                color: white;
+                font-weight: bold;
+            
+            }
+
+            .main-name {
+                margin-left: 20px;
+            }
+
+            .title {
+                text-align: center;
+                font-size: 40px;
+                font-weight: bold;
+            }
+
+            .subtitle {
+                text-align: center;
+                font-size: 30px;
+                font-weight: 250;
+            }
+
+            .main{
+                width: 800px;
+                margin: auto;
+                margin-top: 40px;
+            }
+
+            .prices {
+                margin-top: 100px;
+            }
+
+            .price-item {
+                width: 600px;
+                height: 700px;
+                border: 1px solid black;
+                margin: 100px;
+                border-radius: 4px;
+                text-align: center;
+            }
+
+            .price-item-price {
+                font-size: 40px;
+                font-weight: bold;
+                padding: 30px;
+            }
+
+            .price-item-title{
+                font-size: 30px;
+                background: rgb(146, 226, 203);
+                height: 50px;
+                line-height: 50px;
+                border-bottom: 1px solid black;
+            }
+
+            .price-item-detail{
+                padding: 20px;
+                font-size: 20px;
+            }
+
+            .price-item-buttom {
+                padding: 20px;
+                font-size: 20px;
+                line-height: 10px;
+                border-radius: 20px;
+                color: black;
+                background-color:rgb(218, 237, 231);
+                background-image: none;
+                border-color: white;
+            }
+        </style>
+    </head>
+
+    <body> 
+        <div class="nav">
+            <div class="nav-item">단국대학교</div>
+        </div>
+        
+        <div class="main">
+            <div class="title">
+                오픈소스 과제 페이지
+            </div>
+
+            <div class="subtitle">
+                이름: 손영서 <br>
+                학번: 32212202
+            </div>
+
+            <div class="prices">
+                <div class="price-item">
+                    <div class="price-item-title">
+                        자기소개
+                    </div>
+    
+                    <div class="price-item-price">
+                        <img src="./img/image1.jpg" width="300px", height="200px">
+                    </div>
+    
+                    <div class="price-item-detail">
+                        이름: 손영서 <br>
+                        학번: 32212202 <br>
+                        학과: 산업보안학과 <br>
+                        mbti: isfp <br> 
+                        획득한 자격증: 네트워크 관리사 <br>
+                        취미: 모바일 게임, 그림, 운동 <br>
+                        이메일: location0@naver.com <br><br>
+
+                        바다 생물을 좋아하기 때문에 해파리 사진을 첨부했습니다.
+                       
+                    </div>
+    
+                    <button class="price-item-buttom">
+                        <a href="https://www.youtube.com/watch?v=9bzQYKm3xTA">제가 좋아하는 해파리 영상을 보고 싶다면 여기를 클릭하세요</a>
+                    </button>
+                </div>
+    
+                <div class="price-item">
+                    <div class="price-item-title">
+                        좋아하는 것
+                    </div>
+                    
+                    <div class="price-item-price">
+                        <img src="./img/like1.jpg" width="300px", height="200px">
+                    </div>
+    
+                    <div class="price-item-detail">
+                        1000피스 퍼즐, 큐브, 스도쿠, 와이어 퍼즐 등 <br>
+                        종류를 가리지 않고 퍼즐 푸는 것을 좋아합니다. <br><br>
+                        스도쿠, 십자말 풀이, 체스, 오셀로 등 <br>
+                        보드게임을 매우 좋아해서 <br> 
+                        주로 보드게임 카페에서 서식 중입니다. <br><br>
+                        그 이외에는 추리소설과 <br> 
+                        영국 드라마 빅뱅이론을 좋아합니다.
+                    </div>
+                    <button class="price-item-buttom">
+                        <a href="https://www.youtube.com/watch?v=rSz3MOz6QR8">빅뱅이론 유튜브 보기</a>
+                    </button>
+                </div>
+    
+                <div class="price-item">
+                    <div class="price-item-title">
+                        학과 선택의 계기
+                    </div>
+    
+                    <div class="price-item-price">
+                        <img src="./img/image3.jpg" width="400px", height="300px">
+                    </div>
+    
+                    <div class="price-item-detail">
+                    제가 선택한 산업보안학과는 정보기술 보호 방법과 <br>
+                    보안 문제를 해결하는 방법을 배우는 학과입니다. <br><br>
+                    이전에 랜섬웨어로 노트북의 모든 파일을 날린적이 있는데 <br>
+                    그 날 이후로 파일 복구와 보안 등에 관심이 생기면서 <br>
+                    해당 학과를 선택하게 되었습니다.
+                    </div>
+    
+                    <button class="price-item-buttom">
+                        <a href="https://www.dankook.ac.kr/web/kor/ej09">산업보안학과 홈페이지 가기</a>
+                    </button>
+                </div>
+            </div>    
+        </div>
+        
+    </body>
+</html>
